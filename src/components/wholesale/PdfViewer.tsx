ab7b0635e19c3b9015deca57dbf43f0bbdff61f2
jxrz <jarzinyolo@gmail.com>
2025-05-21 16:55:24 -0600

    pdfjs cambiado

--- src/components/wholesale/PdfViewer.tsx
+++ src/components/wholesale/PdfViewer.tsx
@@ -4,7 +4,7 @@ import { Download, FileText, ArrowLeft, ArrowRight } from 'lucide-react';
 import { Document, Page, pdfjs } from 'react-pdf';
 
 // Workaround for pdf.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.min.js`;
 
 const PdfViewer: React.FC = () => {
   const { t } = useTranslation();
